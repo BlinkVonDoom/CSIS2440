bfe70c3287316e18820554738b6b1f733762427b
blinkvondom <blynk.rankin@gmail.com>
2019-10-08 17:28:09 -0600

    CE06 coursework

---
+++ CSIS2440/CE06/DataBaseConnection.php
@@ -0,0 +1,21 @@
+<?php
+
+$host="localhost";
+$user="root";
+$password="DBAdmin";
+$dbname="CSIS2440";
+
+$con = new mysqli($host, $user, $password, $dbname)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+
+if($con->connection_error == FALSE) {
+  echo "<h2>We Connected</h2>";
+} else {
+  echo $con->connection_error;
+}
+
+print_r($con);
+
+
+
