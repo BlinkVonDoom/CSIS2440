662f10104373aa8146f87f38ec158d0c92bea227
blinkvondom <blynk.rankin@gmail.com>
2019-10-10 18:55:37 -0600

    CE07

---
+++ CSIS2440/CE07/Update.php
@@ -0,0 +1,18 @@
+<?php
+
+$update = "UPDATE `CSIS2440`.`Planets` SET `Active` = 'Y' ";
+if($desc != "A short description of the planet"){
+ $update .= ", `PlanetDesc` = '$desc'"; 
+}
+
+$update .= "WHERE (`PlanetName` = '" .$name . "')";
+  $success = $con->query($update);
+
+  if($success == FALSE) {
+    $failmess = "Whole query" . $insert . "<br>";
+    echo $failmess;
+    print('Invalid query: '.mysqli_error($con). '<br>');
+  } else {
+    echo $name . " was given a space station. <br>";
+  }
+
